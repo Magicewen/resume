68211347eb990c453547eaaff7f7dfc8f9550ec1
vince <magicwen1988@gmail.com>
2018-12-26 23:40:18 +0800

    更新样式

---
+++ js/index.js
@@ -0,0 +1,9 @@
+portfolio1.onclick = function(){
+    portfolioBar.className = 'bar bar-1';
+};
+portfolio2.onclick = function(){
+    portfolioBar.className = 'bar bar-2';
+};
+portfolio3.onclick = function(){
+    portfolioBar.className = 'bar bar-3';
+};
